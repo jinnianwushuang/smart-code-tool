ce37caada0c58b92018c4046c50d3e58a427a82f
jinnian <jinnian@123.cc>
2026-02-27 15:06:27 +0700

    构建配置

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+// Your site is live at https://jinnianwushuang.github.io/smart-code-tool/
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/smart-code-tool/', // 例如：'/my-vue-app/'
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      src: fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
