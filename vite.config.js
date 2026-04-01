e37ae4618fd07b4e67d490deadab3aa6925d091d
jinnian <jinnian@123.cc>
2026-04-01 23:39:57 +0700

    曼谷时区

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import dayjs from 'dayjs'
 import Markdown from 'unplugin-vue-markdown/vite'
 import { createHighlighter } from 'shiki'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+
+// 1. 扩展插件
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
 // https://quasar.dev/start/vite-plugin
 // Your site is live at https://jinnianwushuang.github.io/smart-code-tool/
 // https://vite.dev/config/
@@ -14,7 +21,7 @@ export default defineConfig({
   base: '/smart-code-tool/', // 例如：'/my-vue-app/'
   define: {
     // 注入全局变量
-    __APP_BUILD_TIME__: JSON.stringify(dayjs().format('YYYY-MM-DD HH:mm:ss')),
+    __APP_BUILD_TIME__: JSON.stringify(dayjs().tz('Asia/Bangkok').format('YYYY-MM-DD HH:mm:ss Z')),
   },
   plugins: [
     vue({
